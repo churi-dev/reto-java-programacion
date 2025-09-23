5d2d4c26b0a807b9f76e597d30a14addaa4c9965
churi-dev <jhon.churivanti@tecsup.edu.pe>
2025-09-23 09:12:57 -0500

    feat: actualizado cadena de caracteres con metodos

--- src/CadenaCaracteres_05/Metodos.java
+++ src/CadenaCaracteres_05/Metodos.java
@@ -15,6 +15,24 @@ public class Metodos {
         System.out.println(miNombre.toUpperCase());
         // minuscula
         System.out.println(miNombre.toLowerCase());
+
+        // posicion de un valor por indice
+        var valorIndice = miNombre.indexOf("Churivanti");
+        System.out.println(valorIndice);
+
+        // acceder a un caracter especifico de un valor almacenado
+        System.out.println(miNombre.charAt(0));
+
+        // comparas valores string usando equals()
+        var nombre = "Jhon Churivanti";
+        var nombre2 = " Churivanti";
+
+        System.out.println(miNombre.equals(miNombre)); // boolean
+
+        // remover espacios de un valor
+        System.out.println(" [" + nombre2.trim() + "]");
+        
+        System.out.println(" [" + nombre2.trim() + "]");
     }
 
 
